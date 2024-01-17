a0160bdb50205c7256399e2ba1e62c462af2ee7a
Hamza Shahid <hammzzeeee@gmail.com>
2024-01-17 22:07:38 +0000

    Utility functions for formatting currency and string

--- src/components/SingleApplication/SingleApplication.tsx
+++ src/components/SingleApplication/SingleApplication.tsx
@@ -1,27 +1,6 @@
-import React from 'react'
 import styles from './SingleApplication.module.css'
 
-const formatToGBP = (amount: string) => {
-  const numericAmount = Number(amount)
-  if (isNaN(numericAmount)) {
-    throw new Error('Invalid amount: unable to convert to number.')
-  }
-
-  return new Intl.NumberFormat('en-GB', {
-    style: 'currency',
-    currency: 'GBP',
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0
-  }).format(numericAmount)
-}
-
-const formatDate = (dateString: string) => {
-  const date = new Date(dateString)
-  const day = String(date.getDate()).padStart(2, '0')
-  const month = String(date.getMonth() + 1).padStart(2, '0')
-  const year = date.getFullYear()
-  return `${day}-${month}-${year}`
-}
+import { formatDate, formatToGBP } from '../../utils/utils'
 
 const SingleApplication = ({ application }) => {
   const { company, first_name, last_name, email, loan_amount, date_created, expiry_date } = application
---
+++ src/utils/utils.ts
@@ -0,0 +1,21 @@
+export const formatToGBP = (amount: string) => {
+  const numericAmount = Number(amount)
+  if (isNaN(numericAmount)) {
+    throw new Error('Invalid amount: unable to convert to number.')
+  }
+
+  return new Intl.NumberFormat('en-GB', {
+    style: 'currency',
+    currency: 'GBP',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0
+  }).format(numericAmount)
+}
+
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString)
+  const day = String(date.getDate()).padStart(2, '0')
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const year = date.getFullYear()
+  return `${day}-${month}-${year}`
+}
